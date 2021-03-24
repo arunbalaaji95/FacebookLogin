a772855d54603d685a84c760692574b38c09a6bf
arunbalaaji95 <arunbalaaji95@gmail.com>
2021-03-24 14:50:15 +0530

    message

--- src/org/sam/ample.java
+++ src/org/sam/ample.java
@@ -4,6 +4,10 @@ public class ample {
 	public static void main(String[] args) {
 		
 		System.out.println("arun");
+		int a=10;
+		int b=10;
+		System.out.println(a+b);
+		System.out.println("succeed");
 	}
 
 }
